032d6cb1698695958996a37581bf64ca1dcf6d53
Tomasz <edisoner97@gmail.com>
2018-09-08 17:14:21 +0200

    Upload

--- js/external.js
+++ js/external.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var rellax = new Rellax('.rellax', {
     center: true
 });
@@ -6,33 +5,19 @@ var rellax = new Rellax('.rellax', {
 document
     .getElementsByClassName("arrows")[0]
     .addEventListener("click", function (e) {
-=======
-document
-    .getElementsByClassName("arrows")[0]
-    .addEventListener("click", function(e) {
->>>>>>> a830982eb84b1a0cad3a9d834a47c79247088b0d
         document.getElementsByClassName("about")[0].scrollIntoView({
             behavior: "smooth",
             block: "start",
             inline: "nearest"
-<<<<<<< HEAD
         })
-=======
-        });
->>>>>>> a830982eb84b1a0cad3a9d834a47c79247088b0d
     });
 
 typeout(
     ".typeout",
     ["a programmer", "a computer-scientist", "a designer", "a developer"],
     {
-<<<<<<< HEAD
         numLoops: 12,
         callback: function (el) {
-=======
-        numLoops: 3,
-        callback: function(el) {
->>>>>>> a830982eb84b1a0cad3a9d834a47c79247088b0d
             el.innerHTML += ".";
         }
     }
@@ -41,7 +26,4 @@ document.getElementById(
     "copyright-date"
 ).textContent = new Date().getFullYear();
 document.body.style.opacity = 1;
-<<<<<<< HEAD
 AOS.init({ disable: 'phone' });
-=======
->>>>>>> a830982eb84b1a0cad3a9d834a47c79247088b0d
