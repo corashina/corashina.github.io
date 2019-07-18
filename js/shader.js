e4649c9ce5f9d66ddf83f85ddf92b8fc25db43a6
unknown <edisoner97@gmail.com>
2019-07-18 14:50:27 +0200

    Remove barba

--- js/shader.js
+++ js/shader.js
@@ -1,7 +1,201 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-117328709-1');
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "UA-117328709-1");
+
+document.querySelector('#nav-home').addEventListener('click', () => document.querySelector('.contact').scrollIntoView({behavior: "smooth"}))
+document.querySelector('#nav-about').addEventListener('click', () => document.querySelector('.about').scrollIntoView({behavior: "smooth"}))
+document.querySelector('#nav-work').addEventListener('click', () => document.querySelector('.projects').scrollIntoView({behavior: "smooth"}))
+document.querySelector('#nav-contact').addEventListener('click', () => document.querySelector('.contact').scrollIntoView({behavior: "smooth"}))
+
+!(function n(e, t, r) {
+  function o(u, f) {
+    if (!t[u]) {
+      if (!e[u]) {
+        var a = "function" == typeof require && require;
+        if (!f && a) return a(u, !0);
+        if (i) return i(u, !0);
+        var c = new Error("Cannot find module '" + u + "'");
+        throw ((c.code = "MODULE_NOT_FOUND"), c);
+      }
+      var l = (t[u] = { exports: {} });
+      e[u][0].call(
+        l.exports,
+        function(n) {
+          var t = e[u][1][n];
+          return o(t ? t : n);
+        },
+        l,
+        l.exports,
+        n,
+        e,
+        t,
+        r
+      );
+    }
+    return t[u].exports;
+  }
+  for (
+    var i = "function" == typeof require && require, u = 0;
+    u < r.length;
+    u++
+  )
+    o(r[u]);
+  return o;
+})(
+  {
+    1: [
+      function(n, e, t) {
+        !(function(n, r) {
+          "function" == typeof define && define.amd
+            ? define(r)
+            : "object" == typeof t
+            ? (e.exports = r())
+            : (n.aqueue = r());
+        })(this, function() {
+          function n() {}
+          var e = function(e, t) {
+            function r() {
+              u ||
+                ((u = !0),
+                setTimeout(function() {
+                  f();
+                }, 0));
+            }
+            var o = [],
+              i = -1,
+              e = e || n,
+              t = t || !1,
+              u = !1,
+              f = function() {
+                var n = [].slice.call(arguments),
+                  r = n.shift(),
+                  a = null;
+                return r || !u || (i === o.length - 1 && !t)
+                  ? (r && e.apply(this, [].slice.call(arguments)),
+                    void (u = !1))
+                  : ((i = (i + 1) % o.length),
+                    (a = o[i]),
+                    (n = n.concat(a[1])),
+                    n.push(f),
+                    void a[0].apply(f, n));
+              },
+              a = function() {
+                var n = [].slice.call(arguments),
+                  e = n.shift();
+                return e && "function" == typeof e && (o.push([e, n]), r()), a;
+              };
+            return a;
+          };
+          return e;
+        });
+      },
+      {}
+    ],
+    2: [
+      function(n) {
+        (function(e) {
+          function t(n, e) {
+            var t = {};
+            for (var r in n)
+              n.hasOwnProperty(r) &&
+                (t[r] = "undefined" != typeof e[r] ? e[r] : n[r]);
+            return t;
+          }
+          function r(n, e) {
+            n.className += " " + e;
+          }
+          var o = n("aqueue"),
+            i = {
+              interval: 3e3,
+              completeClass: "typeout-complete",
+              callback: function() {},
+              numLoops: 1,
+              max: 110,
+              min: 40
+            },
+            u = function(n, e, u) {
+              function f() {
+                var n = p.innerHTML.trim();
+                "" !== n && ((m = !1), u.words.unshift(n), (p.innerHTML = "")),
+                  a();
+              }
+              function a() {
+                var n = 0,
+                  e = u.words.length;
+                u.words.forEach(function() {
+                  return (
+                    h(c, u.words[n]),
+                    n === e - 1
+                      ? (y++,
+                        1 / 0 !== y && y === u.numLoops
+                          ? h(d, null)
+                          : (h(v, u.interval)(l), a()))
+                      : (h(v, u.interval)(l), n++, void (n === e && (n = 0)))
+                  );
+                });
+              }
+              function c(n, e) {
+                var t = 0,
+                  r = n.length,
+                  o = setInterval(function() {
+                    r > t
+                      ? ((p.innerHTML += n[t]), t++)
+                      : (window.clearInterval(o), e());
+                  }, s());
+              }
+              function l(n) {
+                var e = setInterval(function() {
+                  var t = p.innerHTML,
+                    r = t.length;
+                  r || (clearInterval(e), n()),
+                    (p.innerHTML = t.substring(0, r - 1));
+                }, s());
+              }
+              function s() {
+                var n = u.max,
+                  e = u.min;
+                return Math.floor(Math.random() * (n - e + 1) + e);
+              }
+              function v(n, e) {
+                setTimeout(function() {
+                  e();
+                }, n);
+              }
+              function d() {
+                r(p, u.completeClass), u.callback(p);
+              }
+              (u = t(i, u || {})), (u.words = e);
+              var p = document.querySelector(n),
+                m = !0,
+                h = o(),
+                y = 0;
+              f();
+            };
+          e.typeout = u;
+        }.call(
+          this,
+          "undefined" != typeof global
+            ? global
+            : "undefined" != typeof self
+            ? self
+            : "undefined" != typeof window
+            ? window
+            : {}
+        ));
+      },
+      { aqueue: 1 }
+    ]
+  },
+  {},
+  [2]
+);
+typeout(".typeout", ["a student", "an engineer", "a designer", "a developer"], {
+  numLoops: 12,
+  callback: el => (el.innerHTML += ".")
+});
 
 const fShader = `
 uniform float   u_amplitude;
@@ -123,102 +317,121 @@ void main() {
 
 var camera, scene, renderer, composer, container, plane;
 var clock = new THREE.Clock();
-var uniforms = { u_amplitude: { value: 300.0 }, u_frequency: { value: 0.005 }, u_time: { value: 0.0 } }
-var mouseX = 0, mouseY = 0;
-var windowHalfX = window.innerWidth / 2, windowHalfY = window.innerHeight / 2;
+var uniforms = {
+  u_amplitude: { value: 300.0 },
+  u_frequency: { value: 0.005 },
+  u_time: { value: 0.0 }
+};
+var mouseX = 0,
+  mouseY = 0;
+var windowHalfX = window.innerWidth / 2,
+  windowHalfY = window.innerHeight / 2;
 
 init();
 animate();
 
 function init() {
+  scene = new THREE.Scene();
 
-    scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
+  camera.position.set(0, 0, 1800);
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.set(0, 0, 1800);
+  plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2000, 2000, 400, 400),
+    new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: vShader,
+      fragmentShader: fShader,
+      side: THREE.BackSide,
+      wireframe: true
+    })
+  );
+  plane.position.z = 1000;
 
-    plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000, 400, 400), new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        vertexShader: vShader,
-        fragmentShader: fShader,
-        side: THREE.BackSide,
-        wireframe: true,
-    }));
-    plane.position.z = 1000;
-    
-    scene.add(plane);
+  scene.add(plane);
 
-    renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas') });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.gammaInput = renderer.gammaOutput = true;
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("canvas")
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.gammaInput = renderer.gammaOutput = true;
 
-    var effectVignette = new THREE.ShaderPass(THREE.VignetteShader);
-    effectVignette.uniforms["offset"].value = 0.95;
-    effectVignette.uniforms["darkness"].value = 1.6;
-    effectVignette.renderToScreen = true;
+  var effectVignette = new THREE.ShaderPass(THREE.VignetteShader);
+  effectVignette.uniforms["offset"].value = 0.95;
+  effectVignette.uniforms["darkness"].value = 1.6;
+  effectVignette.renderToScreen = true;
 
-    composer = new THREE.EffectComposer(renderer);
-    composer.addPass(new THREE.RenderPass(scene, camera));
-    composer.addPass(new THREE.ShaderPass(THREE.HorizontalBlurShader));
-    composer.addPass(new THREE.ShaderPass(THREE.VerticalBlurShader));
-    composer.addPass(new THREE.ClearMaskPass());
+  composer = new THREE.EffectComposer(renderer);
+  composer.addPass(new THREE.RenderPass(scene, camera));
+  composer.addPass(new THREE.ShaderPass(THREE.HorizontalBlurShader));
+  composer.addPass(new THREE.ShaderPass(THREE.VerticalBlurShader));
+  composer.addPass(new THREE.ClearMaskPass());
 
-    composer.addPass(effectVignette);
+  composer.addPass(effectVignette);
 
-    window.addEventListener('resize', onWindowResize, false);
+  window.addEventListener("resize", onWindowResize, false);
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    camera.position.x += ( mouseX - camera.position.x ) * 0.01;
-    camera.position.y += ( -mouseY - camera.position.y ) * 0.01;
-    camera.lookAt( new THREE.Vector3(0,0,0) );
+  camera.position.x += (mouseX - camera.position.x) * 0.01;
+  camera.position.y += (-mouseY - camera.position.y) * 0.01;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    plane.material.uniforms.u_time.value += clock.getDelta() / 4;
+  plane.material.uniforms.u_time.value += clock.getDelta() / 4;
 
-    composer.render(clock.getDelta());
+  composer.render(clock.getDelta());
 }
 
 var mX, mY, distance;
 
-window.addEventListener('mousemove', (e) => {
+window.addEventListener("mousemove", e => {
+  mouseX = e.clientX - windowHalfX;
+  mouseY = e.clientY - windowHalfY;
 
-    mouseX = ( e.clientX - windowHalfX );
-    mouseY = ( e.clientY - windowHalfY );
+  mX = e.pageX;
+  mY = e.pageY;
 
-    mX = e.pageX;
-    mY = e.pageY;
+    document.querySelectorAll(".image").forEach(el => {
+      elem = el.getBoundingClientRect();
+      distance = Math.floor(
+        Math.sqrt(
+          Math.pow(mX - (elem.left + elem.width / 2), 2) +
+            Math.pow(mY - (elem.top + elem.height / 2), 2)
+        )
+      );
+       
+      let val = 400 / Math.min(Math.max(distance, 100), 1000);
+      el.setAttribute("style", `-webkit-filter:grayscale(${distance / 100}%); opacity: ${val}`);
+    });
 
-    if (Barba.Pjax.getCurrentUrl().includes('about')) {
-        document.querySelectorAll('.image').forEach(el => {
+    let titles = document.querySelectorAll(".title");
 
-            elem = el.getBoundingClientRect();
-            distance = Math.floor(Math.sqrt(Math.pow(mX - (elem.left + (elem.width / 2)), 2) + Math.pow(mY - (elem.top + (elem.height / 2)), 2)));
-
-            let val = 400 / Math.min(Math.max(distance, 100), 1000);
-            el.setAttribute("style", `-webkit-filter:grayscale(${distance / 10}%); opacity: ${val}`);
-        })
-    } else if (Barba.Pjax.getCurrentUrl().includes('work')) {
-
-        let titles = document.querySelectorAll('.title');
-     
-        document.querySelectorAll('.projects-item').forEach((el, i) => {
-
-            elem = el.getBoundingClientRect();
-            distance = Math.floor(Math.sqrt(Math.pow(mX - (elem.left + (elem.width / 2)), 2) + Math.pow(mY - (elem.top + (elem.height / 2)), 2)));
-
-            let val = 100 / Math.min(Math.max(distance, 100), 1000);
-            el.setAttribute("style", `filter: grayscale(${ distance / 10}%); -webkit-filter:grayscale(${distance / 10}%); opacity: ${val}`);
-            titles[i].setAttribute("style", `opacity: ${distance / window.innerWidth}`);
-        })
-    }
-
-})
+    document.querySelectorAll(".work-item").forEach((el, i) => {
+      elem = el.getBoundingClientRect();
+      distance = Math.floor(
+        Math.sqrt(
+          Math.pow(mX - (elem.left + elem.width / 2), 2) +
+            Math.pow(mY - (elem.top + elem.height / 2), 2)
+        )
+      );
+            
+      let val = 100 / Math.min(Math.max(distance, 100), 1000);
+      el.setAttribute("style", `filter: grayscale(${distance / 10}%); -webkit-filter:grayscale(${distance / 10}%); opacity: ${val}`);
+      titles[i].setAttribute("style",`opacity: ${distance / window.innerWidth}`);
+    });
+  
+});
