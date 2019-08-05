b648a52aea202c67b1d1f3e4d5161fe1c5f635a0
unknown <edisoner97@gmail.com>
2019-08-05 21:28:12 +0200

    Change font

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,51 +1,41 @@
 import React from 'react'
 import * as THREE from 'three'
 import { vShader, fShader } from './utils/shader'
-import styles from './styles/canvas.module.scss'
 
 class Canvas extends React.Component {
   constructor(props) {
     super(props);
 
-    this.update = this.update.bind(this)
-    this.resize = this.resize.bind(this)
-    this.handleMouseMove = this.handleMouseMove.bind(this)
+    this.update = this.update.bind(this);
+    this.resize = this.resize.bind(this);
 
-    this.mouseX = 0
-    this.mouseY = 0
+    this.colorMain = 0xcccccc;
+    this.colorBg = 0x222222;
+    this._lastTime = Date.now();
 
-    this.colorMain = 0xcccccc
-    this.colorBg = 0x222222
-    this.lastTime = Date.now()
+    this._lastPageY = 0;
+    this._transitionStartTime = 0;
+    this._transitionDuration = 0;
+    this._transitionIsBackward = false;
 
-    this.transitionStartTime = 0
-    this._transitionIsBackward = false
-
-    if (typeof window !== "undefined") window.CANVAS_BACKGROUND = this;
+    if (typeof window !== "undefined")  window.CANVAS_BACKGROUND = this;
 
   }
 
   componentDidMount() {
     window.addEventListener("resize", this.resize);
-    window.addEventListener("mousemove", this.handleMouseMove);
     this.init();
   }
 
   componentWillUnmount() {
         cancelAnimationFrame(this.frameId);
-        this.frameId = undefined;
-        this.mount.removeChild(this.renderer.domElement);
-        window.removeEventListener("resize", this.resize);
-        window.removeEventListener("mousemove", this.handleMouseMove);
+    this.frameId = undefined;
+    this.mount.removeChild(this.renderer.domElement);
+    window.removeEventListener("resize", this.resize);
   }
 
   setColors(colorMain, colorBg) {
     this.renderer.setClearColor(colorBg);
-  }
-
-  handleMouseMove(e) {
-    this.mouseX = ( e.clientX - window.innerWidth / 2 )
-    this.mouseY = ( e.clientY - window.innerHeight / 2)
   }
 
   init() {
@@ -80,49 +70,54 @@ class Canvas extends React.Component {
 
   initScene() {
           
-    this.scene = new THREE.Scene()
-    this.clock = new THREE.Clock()
-    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000)
-    this.camera.position.set(0, 0, 1000)
+        this.scene = new THREE.Scene()
+        this.clock = new THREE.Clock()
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000)
+        this.camera.position.set(0, 0, 1800)
 
-    var uniforms = {
-        u_amplitude: { value: 300.0 },
-        u_frequency: { value: 0.005 },
-        u_time: { value: 0.0 }
-    };
+        var uniforms = {
+            u_amplitude: { value: 300.0 },
+            u_frequency: { value: 0.005 },
+            u_time: { value: 0.0 }
+        };
 
-    this.plane = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry(4000, 4000, 80, 80),
-        new THREE.ShaderMaterial({
-            uniforms: uniforms,
-            vertexShader: vShader,
-            fragmentShader: fShader,
-            side: THREE.BackSide,
-            wireframe: true
-        })
-    )
-    this.scene.add(this.plane)
+        this.plane = new THREE.Mesh(
+            new THREE.PlaneBufferGeometry(2000, 2000, 40, 40),
+            new THREE.ShaderMaterial({
+                uniforms: uniforms,
+                vertexShader: vShader,
+                fragmentShader: fShader,
+                side: THREE.BackSide,
+                wireframe: true
+            })
+        )
+        this.plane.position.z = 1000
+
+        this.scene.add(this.plane)
 
   }
 
   updateScene() {
         
-        this.camera.position.x += (-this.mouseX - this.camera.position.x) * 0.01;
-        this.camera.position.y += (this.mouseY - this.camera.position.y) * 0.01;
+        // this.camera.position.x += (mouseX - camera.position.x) * 0.01;
+        // this.camera.position.y += (-mouseY - camera.position.y) * 0.01;
         this.camera.lookAt(new THREE.Vector3(0, 0, 0))
 
         this.plane.material.uniforms.u_time.value += this.clock.getDelta() / 4;
   }
 
   triggerTransition(duration) {
-    this.transitionStartTime = this.lastTime
+    this._transitionStartTime = this._lastTime;
+    this._transitionDuration = duration;
   }
 
   render() {
     return (
       <div
-        className={styles.wrap}
-        ref={(mount) => {this.mount = mount}}
+        id="canvas-wrap"
+        ref={(mount) => {
+          this.mount = mount;
+        }}
       />
     );
   }
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Nav from './Nav'
-import Footer from './Footer'
-
-import styles from './styles/layout.module.scss'
+import Navigation from './Navigation'
 
 export default ({ children, location, title, width }) => {
 
-  const style = width ? { maxWidth: `${width}px` } : {};
+  const footer = (
+    <div className="footer">
+      <p>
+        Copyright &copy; {new Date().getFullYear()} Tomasz Zielinski
+      </p>
+    </div>
+  );
 
+  const style = width ? { maxWidth: `${width}px` } : {};
   return (
-    <div className={styles.layout} style={style}>
-      <Helmet title={location.pathname.split('/').pop()}>
-        <meta name='description' title={title} content='Tomasz Zielinski Portfolio Website' />
+    <div className="layout" style={style}>
+      <Helmet title={location.pathname.slice(1, -1)}>
+        <meta name="description" content='Tomasz Zielinski Portfolio Website' />
       </Helmet>
-      <Nav location={location} />
-      <div className='content'>{children}</div>
-      <Footer />
+      <Navigation location={location} />
+      <div className="content">{children}</div>
+      {footer}
     </div>
   );
 };
